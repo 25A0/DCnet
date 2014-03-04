c3534f166e65f3664064cad53f6a10f5da7768d4
25a0 <mneikes@gmx.de>
2014-03-04 04:30:08 +0100

    Working environment
    Added some basic files that make it easier to run the application from the command line.

---
+++ src/component/ClientMain.java
@@ -0,0 +1,12 @@
+package component;
+
+import java.util.Arrays;
+
+import cli.CLI;
+
+public class ClientMain {
+	public static void main(String... args) {
+		System.out.println("Client started");
+		new CLI();
+	}
+}
---
+++ src/component/ServerMain.java
@@ -0,0 +1,9 @@
+package component;
+
+import dcp.server.DCpServer;
+
+public class ServerMain {
+	public static void main(String... args) {
+		new DCpServer();
+	}
+}
