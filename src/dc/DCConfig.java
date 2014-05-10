f2b2ab86f75270666a46a68ab294deeb5fa1b591
Moritz Neikes <m.neikes@student.ru.nl>
2014-05-10 18:17:21 +0200

    Introducing fingerprint scheduling
    Fingerprint scheduling is a new fast and efficient scheduling algorithm.
    Not final, still vulnerable to DOS attacks

--- src/dc/DCConfig.java
+++ src/dc/DCConfig.java
@@ -1,5 +1,9 @@
 package dc;
 
+import dc.scheduling.FingerprintScheduler;
+import dc.scheduling.PrimitiveScheduler;
+import dc.scheduling.Scheduler;
+
 public class DCConfig {
 	
 	public static final String VERSION = "0.1";
@@ -8,5 +12,25 @@ public class DCConfig {
 	// So, a value of 2 means that a station has to share keys with to 2 other stations, leading to networks
 	// of a minimum size of 3.
 	public static final int MIN_NUM_KEYS = 2;
+
+	public static final SchedulingMethod schedulingMethod = SchedulingMethod.FINGERPRINT;
+
+	public enum SchedulingMethod {
+		PRIMITIVE(){
+			@Override
+			public Scheduler getScheduler() {
+				return new PrimitiveScheduler();
+			}
+		},
+		FINGERPRINT(){
+			@Override
+			public Scheduler getScheduler() {
+				return new FingerprintScheduler(32);
+			}
+		};
+
+		public abstract Scheduler getScheduler();
+
+	}
 	
 }
